a912baf22dd1332f1fb686491c29d0330eef19ec
blaiseonitch <basohblaise23@gmail.com>
2026-01-27 15:57:58 +0100

    got stats saving working

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,20 @@ Player _p;
 int main(void) {
   InitWindow(800, 450, "raylib [core] example - basic window");
 
+  // create file to write player properties to
   std::ofstream file;
-  file.open("config.txt");
+  file.open("config.ini");
+
   while (!WindowShouldClose()) {
     Update();
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText("Congrats!", 190, 200, 20, LIGHTGRAY);
-    DrawRectangle(_p.posX, _p.posY, _p.width, _p.height, RED);
+    DrawRectangle((_p.posX), (_p.posY), _p.width, _p.height, RED);
     EndDrawing();
   }
 
+  // save player properties to file and close file
   file << "PosY = " << _p.posY << "\n";
   file << "PosX = " << _p.posX << "\n";
   file.close();
@@ -41,16 +44,16 @@ int main(void) {
 // This function runs every frame
 void Update() {
 
-  if (IsKeyDown(KEY_W)) {
+  if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) {
     _p.posY -= _p._speed;
   }
-  if (IsKeyDown(KEY_S)) {
+  if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) {
     _p.posY += _p._speed;
   }
-  if (IsKeyDown(KEY_A)) {
+  if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
     _p.posX -= _p._speed;
   }
-  if (IsKeyDown(KEY_D)) {
+  if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
     _p.posX += _p._speed;
   }
 }
